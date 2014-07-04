063a12a65194011e12ba31b22f808bb12a63003e
Richard Tape <richard.tape@ubc.ca>
2014-07-03 17:30:49 -0700

    ADD css themes. Add filter 'wp_side_comments_css_theme' to be able to add different CSS themes

--- wp-side-comments.php
+++ wp-side-comments.php
@@ -70,13 +70,24 @@
 				return;
 			}
 
+			// The theme to load - must be a string of the url to load
+			$theme = apply_filters( 'wp_side_comments_css_theme', CTLT_WP_SIDE_COMMENTS_PLUGIN_URL . 'includes/css/themes/default-theme.css' );
+
 			wp_register_style( 'side-comments-style', CTLT_WP_SIDE_COMMENTS_PLUGIN_URL . 'includes/css/side-comments.css' );
 			wp_register_script( 'side-comments-script', CTLT_WP_SIDE_COMMENTS_PLUGIN_URL . 'includes/js/side-comments.js', array ( 'jquery' ) );
 			wp_register_script( 'wp-side-comments-script', CTLT_WP_SIDE_COMMENTS_PLUGIN_URL . 'includes/js/wp-side-comments.js', array ( 'jquery', 'side-comments-script' ), null, true );
-			
+
 			wp_enqueue_style( 'side-comments-style' );
 			wp_enqueue_script( 'side-comments-script' );
 			wp_enqueue_script( 'wp-side-comments-script' );
+
+			if( $theme && !empty( $theme ) )
+			{
+
+				wp_register_style( 'side-comments-theme', $theme );
+				wp_enqueue_style( 'side-comments-theme' );
+
+			} 
 
 			// Need to get some data for our JS, which we pass to it via localization
 			$data = $this->getCommentsData();
